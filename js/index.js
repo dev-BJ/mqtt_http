ea8e3dbc494c752581b33a19179c563c405ed501
phreaker <tife2111@gmail.com>
2025-06-05 13:39:20 +0100

    added route and method for mqtt devices

--- js/index.js
+++ js/index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
   })
 })
 
+app.post('/mqtt/:cliend_id', (req, res) => {
+  const clientId = req.params.client_id
+  const topic = req.query.topic
+  const message = req.query.message
+  console.log(`Received MQTT message for client ${clientId} on topic ${topic}: ${message}`)
+  res.sendStatus(200)
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
